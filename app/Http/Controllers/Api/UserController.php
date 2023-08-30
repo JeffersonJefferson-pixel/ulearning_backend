e356db2aaca014cb4a27c694efa770bd9ecb091d
JeffersonJefferson-pixel <erenthewizard@gmail.com>
2023-08-31 00:40:38 +0700

    prepare register user for third party
    automatically login user if already registered

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -16,9 +17,12 @@ class UserController extends Controller
         try {
             $validateUser = Validator::make($request->all(), 
             [
+                'avatar' => 'required',
+                'type' => 'required',
+                'open_id' => 'required',
                 'name' => 'required',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required'
+                'email' => 'required',
+                'password' => 'required|min:6'
             ]);
             
             if($validateUser->fails()) {
@@ -29,16 +33,48 @@ class UserController extends Controller
                 ], 401);
             }
 
-            $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => Hash::make($request->password)
-            ]);
+            $validated = $validateUser->validated();
 
+            $map=[];
+            // email, phone, google, facebook, apple
+            $map['type'] = $validated['type'];
+            $map['open_id'] = $validated['open_id'];
+
+            $user = User::where($map)->first();
+
+            // whether user has already logged in
+            if (empty($user->id)) {
+                // save user in db for first time
+                // token ~ id
+                $validated["token"] = md5(uniqid().rand(10000, 99999));
+                $validated["created_at"] = Carbon::now();
+                // encrypt password
+                $validated["password"] = Hash::make($validated["password"]);
+                $userID = User::insertGetId($validated);
+
+                $userInfo = User::where('id', '=', $userID)->first();
+
+                $accessToken = $userInfo->createToken(uniqid())->plainTextToken;
+
+                $userInfo->access_token = $accessToken;
+                
+                User::where('id', '=', $userID)->update(['access_token'=>$accessToken]);
+
+                return response()->json([
+                    'status' => true,
+                    'message' => 'User Created Successfully',
+                    'data' => $userInfo
+                ], 200);
+            }
+
+            // user has logged in
+            $accessToken = $user->createToken(uniqid())->plainTextToken;
+            $user->access_token = $accessToken;
+            User::where('id', '=', $user->id)->update(['access_token'=>$accessToken]);
             return response()->json([
                 'status' => true,
-                'message' => 'User Created Successfully',
-                'token' => $user->createToken("API TOKEN")->plainTextToken
+                'message' => 'User logged in Successfully',
+                'data' => $user
             ], 200);
         } catch(\Throwable $th) {
             return response()->json([
